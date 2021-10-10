fdb4fe0e8e39e223871cb805f4028e1491c226f6
Meheraj <meherajmahmmd@gmail.com>
2021-10-11 02:05:28 +0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Acomplishments from '../components/Acomplishments/Acomplishments';
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
+import Blogs from '../components/Blogs/Blogs';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
@@ -16,6 +17,7 @@ const Home = () => {
       </Section>
       <Projects />
       <Technologies />
+      <Blogs/>
       <Timeline />
       {/* <Acomplishments /> */}
     </Layout>
