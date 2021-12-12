8d9a726ee32dc8105952d3ac1c286631528b01b0
Meheraj <meherajmahmmd@gmail.com>
2021-12-12 12:37:45 +0600

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,45 +16,52 @@ import {
 
 const Footer = () => {
   return (
-      <FooterWrapper id="contact">
-        <LinkList>
-          <LinkColumn>
-            <LinkTitle>Call</LinkTitle>
-            <LinkItem href="tel:+8801814325624">+880 1814 325624</LinkItem>
-          </LinkColumn>
-          <LinkColumn>
-            <LinkTitle>Email</LinkTitle>
-            <LinkItem href="mailto:meherajmahmmd@gmail.com">
-              meherajmahmmd@gmail.com
-            </LinkItem>
-          </LinkColumn>
-        </LinkList>
-        <SocialIconsContainer>
-          <CompanyContainer>
-            <Slogan>If you can dream it, you can do it.</Slogan>
-          </CompanyContainer>
-          <SocialContainer>
-            <SocialIcons
-              href="https://github.com/MeherajUlMahmmud"
-              target="_blank"
-            >
-              <AiFillGithub size="3rem" />
-            </SocialIcons>
-            <SocialIcons
-              href="https://www.linkedin.com/in/meherajulmahmmud/"
-              target="_blank"
-            >
-              <AiFillLinkedin size="3rem" />
-            </SocialIcons>
-            <SocialIcons
-              href="https://www.facebook.com/meherajulmahmmud/"
-              target="_blank"
-            >
-              <AiFillFacebook size="3rem" />
-            </SocialIcons>
-          </SocialContainer>
-        </SocialIconsContainer>
-      </FooterWrapper>
+    <FooterWrapper id="contact">
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+8801814325624">+880 1814 325624</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:meherajmahmmd@gmail.com">
+            meherajmahmmd@gmail.com
+          </LinkItem>
+        </LinkColumn>
+        <LinkColumn><LinkTitle></LinkTitle><LinkItem></LinkItem></LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Resume</LinkTitle>
+          <LinkItem href="https://drive.google.com/file/d/1AXyf2W_NWTZLiiU9Dz_UQgGoPs7ppGfh/view?usp=sharing" target="_blank">
+            Check Out My Resume
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>If you can dream it, you can do it.</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons
+            href="https://github.com/MeherajUlMahmmud"
+            target="_blank"
+          >
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons
+            href="https://www.linkedin.com/in/meherajulmahmmud/"
+            target="_blank"
+          >
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons
+            href="https://www.facebook.com/meherajulmahmmud/"
+            target="_blank"
+          >
+            <AiFillFacebook size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
