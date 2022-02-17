d326ee7c21ee566e055a08ed26fa6561a71115ca
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-02-17 11:14:16 +0600

    changed indentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,27 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Blogs from '../components/Blogs/Blogs';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
-import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+import Acomplishments from "../components/Acomplishments/Acomplishments";
+import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
+import Blogs from "../components/Blogs/Blogs";
+import Hero from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
+import Timeline from "../components/TimeLine/TimeLine";
+import { Layout } from "../layout/Layout";
+import { Section } from "../styles/GlobalComponents";
 
 const Home = () => {
-  return (
-    <Layout>
-      <Section grid>
-        <Hero />
-        <BgAnimation />
-      </Section>
-      <Projects />
-      <Technologies />
-      {/* <Blogs/> */}
-      <Timeline />
-      {/* <Acomplishments /> */}
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Section grid>
+				<Hero />
+				<BgAnimation />
+			</Section>
+			<Projects />
+			<Technologies />
+			{/* <Blogs/> */}
+			<Timeline />
+			{/* <Acomplishments /> */}
+		</Layout>
+	);
 };
 
 export default Home;
